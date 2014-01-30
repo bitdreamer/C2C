83f3abd44e31c36af8d587415a6ac0bec58d0184
Arocho <allygmartinez@gmail.com>
2014-01-30 02:00:57 -0500

    Added Opportunity pages.  Can now add to MajorOpportunity table.

--- admin/addOpportunity.php
+++ admin/addOpportunity.php
@@ -3,19 +3,73 @@
 <title>Classroom to Career Pathways</title>
 </head>
 <body>
-<h1>Classroom to Career Pathways</h1>
+<h1>Add Opportunity</h1>
 <?php
 	include("../included/menu.php");
 	openAddMenu();
 ?>
 <br/>
-<h2>Add New Interest</h2>
-<form action="processInterest.php" method="POST">
+<h2>Add New Opportunity</h2>
+<form action="processOpportunity.php" method="POST">
    <table>
       <tr>
-         <td align="right">Interest</td>
-         <td> <input type="text" name="interest" /> </td>
+         <td align="right">Major</td>
+         <!-- Create page with info within select tags-->
+         <td><select name="major" id="major" required="required">
+				<option value="Accounting">Accounting</option>
+				<option value="Art Education">Art Education</option>
+				<option value="Biology">Biology</option>
+				<option value="Business Administration">Business Administration</option>
+				<option value="Business Administration - Human Resource Management">Business Administration - Human Resource Management</option>
+				<option value="Chemistry">Chemistry</option>
+				<option value="Child Development">Child Development</option>
+				<option value="Communication - Interpersonal Communication">Communication - Interpersonal Communication</option>
+				<option value="Communication - Mass Communication">Communication - Mass Communication</option>
+				<option value="Computer Science">Computer Science</option>
+				<option value="Criminology">Criminology</option>
+				<option value="Dance Studies">Dance Studies</option>
+				<option value="Economics">Economics</option>
+				<option value="Education">Education</option>
+				<option value="Engineering">Engineering</option>
+				<option value="English">English</option>
+				<option value="Environmental Sustainability">Environmental Sustainability</option>
+				<option value="Exercise & Sports Science - Health & Physical Education">Exercise & Sports Science - Health & Physical Education</option>
+				<option value="Exercise & Sports Science - Health & Wellness">Exercise & Sports Science - Health & Wellness</option>
+				<option value="Family & Consumer Sciences">Family & Consumer Sciences</option>
+				<option value="Fashion Merchandising & Design - Merchandising">Fashion Merchandising & Design - Merchandising</option>
+				<option value="Fashion Merchandising & Design - Design">Fashion Merchandising & Design - Design</option>
+				<option value="Food & Nutrition">Food & Nutrition</option>
+				<option value="Graphic Design">Graphic Design</option>
+				<option value="History">History</option>
+				<option value="Interior Design">Interior Design</option>
+				<option value="International Studies">International Studies</option>
+				<option value="Mathematics">Mathematics</option>
+				<option value="Music">Music</option>
+				<option value="Music Education">Music Education</option>
+				<option value="Political Science">Political Science</option>
+				<option value="Political Science - Law & Justice">Political Science - Law & Justice</option>
+				<option value="Public Health">Public Health</option>
+				<option value="Psychology">Psychology</option>
+				<option value="Religious & Ethical Studies">Religious & Ethical Studies</option>
+				<option value="Social Work">Social Work</option>
+				<option value="Sociology">Sociology</option>
+				<option value="Spanish">Spanish</option>
+				<option value="Studio Art">Studio Art</option>
+				<option value="Theatre">Theatre</option>
+			</select></td>
       </tr>
+      
+      <!--Create a drop down list of opportunities-->
+      <tr>
+         <td align="right">Opportunity</td>
+         <td> <input type="text" name="opportunity" required="required"/> </td>
+      </tr>
+      
+      <tr>
+         <td align="right">Link</td>
+         <td> <input type="url" name="link" required="required"/> </td>
+      </tr>
+      
       <tr>
          <td align="right">Submit</td>
          <td> <input type="submit"  name="Submit" value="Submit"/> </td>
@@ -32,7 +86,7 @@
    openDB();
 
 
-	$query="select * from Interest;";
+	$query="select * from MajorOpportunities;";
     $result=mysql_query($query);
    
    tabledump( $result );
---
+++ admin/processOpportunity.php
@@ -0,0 +1,28 @@
+<?php
+   include("..//included/tabledump.php");
+   include("..//included/openDB.php");
+   openDB();
+
+  $major= $_POST['major'];
+   $url= addslashes($_POST['link']);
+   $opportunity= $_POST['opportunity'];
+   
+   $query="SELECT id FROM Major WHERE major='$major';";
+   $result=mysql_query($query);
+   $resultArray=mysql_fetch_array($result);
+   $majorID=$resultArray[0];
+   
+   $query1="INSERT INTO MajorOpportunities "
+          ." set majorID='$majorID' "
+          ." ,opportunity='$opportunity' "
+          ." ,link='$url' "
+          .";";
+          
+   $result=mysql_query($query1);
+   if ($result==0) { noerror( $result ); }
+   //echo "query = $query";
+
+   header("Location: addOpportunity.php");
+   exit;
+   
+?>
