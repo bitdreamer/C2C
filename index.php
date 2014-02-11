3491360f881536fe24a7698886559a91e03f208d
mofitz <fitzgera@email.meredith.edu>
2014-02-10 20:37:02 -0500

    added major, job dropdown menus

--- index.php
+++ index.php
@@ -23,20 +23,21 @@
 
 <div id="body">
 		<?php
-			echo "<div id="."section".">";
+			//echo "<div id="."section".">";
 
    			include("included/tabledump.php");
   			include("included/openDB.php");
   			openDB();
    
-			echo "</br>";
+			//echo "</br>";
 
       		$query="SELECT * FROM Alumna;";
       		$result=mysql_query($query);
    
-   			tabledump( $result );
+   			//tabledump( $result );
    
-			echo "<div id="."section".">";
+			//echo "<div id="."section".">";
+ 
 		?>
 	</p>
 	 <p>
@@ -48,8 +49,62 @@
  		where, and for whom. Many of the key skills you develop in your program are transferable. These Career Pathways are here to help you see where your choices might lead you, and to showcase some sets of skills as used for different job functions or in different organizational environments, both physical and virtual.
 
 		In Meredith collage, We offer the following Career Pathways for your consideration. Please note that this is only a partial list of the kinds of career directions you can follow.
-		
-		
+	</p>
+	<form action="majorRequest.php" method="POST">
+	<fieldset id="majorInfo">
+		<legend>What is your Major/Intended Major?</legend>
+			<?php
+				$query = "SELECT id, major FROM Major ORDER BY major"; 
+				$result = mysql_query($query); 
+
+				if(noerror($result))
+				{
+					echo "<select name='major'> \n";
+					
+					$nr = mysql_num_rows($result); 
+					
+					for($i=0; $i<$nr; $i++)
+					{
+						$row = mysql_fetch_array($result);
+						$majorID = $row['id']; 
+						$major = $row['major']; 
+						
+						echo "<option value='$majorID'>$major</option> \n"; 
+					}
+					echo "</select> \n";
+				}
+			?>
+				</br>
+				<input type="submit" value="Go"/>
+	</fieldset>
+	</form>
+
+	<fieldset id="jobInfo">
+		<legend>Which of the following jobs are you most interested in?</legend>
+			<?php
+				$query = "SELECT id, career FROM Job ORDER BY career"; 
+				$result = mysql_query($query); 
+
+				if(noerror($result))
+				{
+					echo "<select name='career'> \n";
+					
+					$nr = mysql_num_rows($result); 
+					
+					for($i=0; $i<$nr; $i++)
+					{
+						$row = mysql_fetch_array($result);
+						$jobID = $row['id']; 
+						$job = $row['career']; 
+						
+						echo "<option value='$jobID'>$job</option> \n"; 
+					}
+					echo "</select> \n";
+				}
+			?>
+				</br>
+				<input type="submit" value="Go"/>
+	</fieldset>
 	
 </div>	
 
