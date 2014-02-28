57d604f13d8a690b57d7fcbf9a98b2e24edf9b92
mofitz <fitzgera@email.meredith.edu>
2014-02-27 22:45:42 -0500

    Cleaned up formatting

--- jobRequest.php
+++ jobRequest.php
@@ -28,12 +28,18 @@
 <?php
 
 	$jobID=addslashes($_GET['career']); 
+	
 	$query="SELECT * FROM Job WHERE id='$jobID';"; 
 	$result=mysql_query($query); 
 	
 	if(noerror($result))
 	{
-		tabledump($result); 
+		$row = mysql_fetch_array($result); 
+		$job = $row['career']; 
+		$des = $row['description']; 
+		
+		echo "<h1>$job</h1> \n"; 
+		echo "<p>$des</p> \n"; 
 	}
 	
 	$q2="SELECT * FROM MajorJob, Major WHERE majorID=id AND jobID='$jobID';";
@@ -41,7 +47,17 @@
 	
 	if(noerror($r2))
 	{
-		tabledump($r2);
+		echo "<h3>Want to be a $job?...Major in:</h3> \n"; 
+		
+		$nr = mysql_num_rows($r2);
+		for($i=0; $i<$nr; $i++)
+		{
+			$row     = mysql_fetch_array($r2); 
+			$major   = $row['major'];
+			$majorID = $row['id']; 
+			
+			echo "<a href='majorRequest.php?major=$majorID'>$major</a> \n"; 
+		}
 	}
 	
 ?>
--- majorRequest.php
+++ majorRequest.php
@@ -29,7 +29,6 @@
 
 	//get and display Major and description 
 	$majorID=addslashes($_GET['major']); 
-
 	$query="SELECT * FROM Major WHERE id='$majorID';"; 
 	$result=mysql_query($query); 
 	
@@ -47,10 +46,11 @@
 		echo "</p> \n"; 
 	}
 	
+	//get and display job options 
 	$q2="SELECT * FROM MajorJob, Job WHERE jobID=id AND majorID='$majorID';";
 	$r2=mysql_query($q2); 
 
- 			echo "<h3> Career Opportunities </h3> \n"; 
+ 			echo "<h3> Career Options </h3> \n"; 
 			
 	if(noerror($r2))
 	{
@@ -58,11 +58,50 @@
 		for($i=0; $i<$nr; $i++)
 		{
 			$row = mysql_fetch_array($r2); 
-			$job = $row['career']; 
-			$jdes= $row['description']; 
+			$job  = $row['career']; 
+			$jdes = $row['description']; 
+			$jobID  = $row['id']; 
 
+			//echo "<a href='jobRequest.php?career=$jobID'>$job</a> \n"; 			
+			
+			echo "<table class='jobs' \n"; 
+echo "	summary='List of career options for a particular major'> \n"; 
+	
+echo "	<colgroup> \n"; 
+echo "	<col class='jobs' span='1' /> \n"; 
+echo "		</colgroup> \n"; 
+echo " 	<thead> \n"; 
+echo " 	<tr> \n"; 
+//echo "	<th>Career Options</th> \n"; 
+echo "  </tr> \n"; 
+echo "  </thead> \n"; 
+
+echo "  <tbody> \n"; 
+echo "  <tr> \n"; 
+echo "	  <th> \n"; 
+echo "		<a href='jobRequest.php?career=$jobID'>$job</a> \n";
+echo "	  </th> \n";
+echo " 	</tr> \n";
+echo " 	</tbody> \n";
+echo "	</table> \n";
+		}
+	}
+	
+	$linkQ="SELECT * FROM MajorLink, Link WHERE MajorLink.linkID=Link.linkID AND majorID='$majorID';"; 		//one linkID from Link table, one from MajorLink table
+	$linkR=mysql_query($linkQ);
+	
+		echo "<h3> Related Websites</h3> \n"; 
+	
+	if(noerror($linkR))
+	{
+		$nr = mysql_num_rows($linkR);
+		for($i=0; $i<$nr; $i++)
+		{
+			$row  = mysql_fetch_array($linkR);
+			$link = $row['link']; 
+		
 			echo "<p> \n";
-			echo "$job: $jdes \n"; 			
+			echo "<a href='$link'> $link </a> \n";
 			echo "</p> \n"; 
 		}
 	}
