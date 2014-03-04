ff2642af9f1119e1e318eba2168d159c3884af23
Rodi <tota_241@hotmail.com>
2014-03-04 17:22:09 -0500

    here is my new style. re-code css, edit new images, add a video(needs editing) and create contact page(under editing). since we agreed in our new style. I am going to work on the rest.

--- index.php
+++ index.php
@@ -1,132 +1,207 @@
-<!-- 
-//main page 
- -->
+<!-- main Page -->
 <html>
-<head> 
-	<link rel= "stylesheet" href="style.css" type="text/css" />
+
+<head>
+	<link rel= "stylesheet"type="text/css"  href="style.css"  />
+	<title> Carrer Pathways</title>
 </head>
 
-<body>
+<!--logo-->
+<body> 	   
+	<div id="darkgray"></div>
+	<div id="logo"></div>
+	<div id="lightgray"></div>
 
-<!-- 
-\\logo
- -->
-<div id="container">
-<div id="logo">
-	     <img src= "images/upperlogo.png" alt="logo"/>
+
+<!-- left list-->
+<div id="links">
+   <ul class="links">
+	   <li><a href="http://www.meredith.edu">Meredith College</a></li>
+	   <li><a href="index.php">Career Pathways</a></li>
+	   <li><a href="login/main_login.php">Admin login</a></li>
+           <li><a href="contact.html">Contact us</a></li>
+   <ul>	
 </div>
+	
+	
 
-<!-- 
-\\ left list
- -->
-<div id="leftNav">
-<ul class="links">
-		<li><a href="http://www.meredith.edu"> Meredith College</a></li>
-		<li><a href="login/main_login.php">Admin Login</a></li>
-</ul>		
-</div>
 
-<!-- 
-//drop down choices for Majors and Jobs
- -->
+
+<div id="content">
+<h1>Career PathWays</h1>
+
+
+
+<!-- drop down choices for Majors and Jobs-->
+
 <div id="body">
+
 		<?php
+
 			//echo "<div id="."section".">";
 
    			include("included/tabledump.php");
+
   			include("included/openDB.php");
+
   			openDB();
+
    
+
 			//echo "</br>";
 
+
+
       		$query="SELECT * FROM Alumna;";
+
       		$result=mysql_query($query);
+
    
+
    			//tabledump( $result );
+
    
+
 			//echo "<div id="."section".">";
+
  
+
 		?>
-	</p>
-	 <p>
-	 	<h1>Career PathWays</h1>
 
-	    These Career Pathways are presented as introductions to different careers and topics, and are intended to be used by students for career planning and course selection
+	<p> You can choose from Majors.</p>
 
-		We encourage you to remain open-minded when considering what you can do for work,
- 		where, and for whom. Many of the key skills you develop in your program are transferable. These Career Pathways are here to help you see where your choices might lead you, and to showcase some sets of skills as used for different job functions or in different organizational environments, both physical and virtual.
 
-		In Meredith collage, We offer the following Career Pathways for your consideration. Please note that this is only a partial list of the kinds of career directions you can follow.
-	</p>
-	
+
+	<!-- major form-->
+
 	<form action="majorRequest.php" method="GET">
+
 	<fieldset id="majorInfo">
+
 		<legend>What is your Major/Intended Major?</legend>
+
 			<?php
+
 				$query = "SELECT id, major FROM Major ORDER BY major"; 
+
 				$result = mysql_query($query); 
 
+
+
 				if(noerror($result))
+
 				{
+
 					echo "<select name='major'> \n";
+
 					
+
 					$nr = mysql_num_rows($result); 
+
 					
+
 					for($i=0; $i<$nr; $i++)
+
 					{
+
 						$row = mysql_fetch_array($result);
+
 						$majorID = $row['id']; 
+
 						$major = $row['major']; 
+
 						
+
 						echo "<option value='$majorID'>$major</option> \n"; 
+
 					}
+
 					echo "</select> \n";
+
 				}
+
 			?>
+
 				</br>
+
 				<input type="submit" value="Go"/>
+
 	</fieldset>
+
 	</form>
+
+	
+
+<p> OR choose from Job</>
+
+<!-- Job Form-->
 
 	<form action="jobRequest.php" method="GET">
+
 	<fieldset id="jobInfo">
+
 		<legend>Which of the following jobs are you most interested in?</legend>
+
 			<?php
+
 				$query = "SELECT id, career FROM Job ORDER BY career"; 
+
 				$result = mysql_query($query); 
 
-				if(noerror($result))
-				{
-					echo "<select name='career'> \n";
-					
-					$nr = mysql_num_rows($result); 
-					
-					for($i=0; $i<$nr; $i++)
-					{
-						$row = mysql_fetch_array($result);
-						$jobID = $row['id']; 
-						$job = $row['career']; 
-						
-						echo "<option value='$jobID'>$job</option> \n"; 
-					}
-					echo "</select> \n";
-				}
-			?>
-				</br>
-				<input type="submit" value="Go"/>
-	</fieldset>
-	</form>
-	
-</div>	
 
+
+				if(noerror($result))
+
+				{
+
+					echo "<select name='career'> \n";
+
+					
+
+					$nr = mysql_num_rows($result); 
+
+					
+
+					for($i=0; $i<$nr; $i++)
+
+					{
+
+						$row = mysql_fetch_array($result);
+
+						$jobID = $row['id']; 
+
+						$job = $row['career']; 
+
+						
+
+						echo "<option value='$jobID'>$job</option> \n"; 
+
+					}
+
+					echo "</select> \n";
+
+				}
+
+			?>
+
+				</br>
+
+				<input type="submit" value="Go"/>
+
+	</fieldset>
+
+	</form>		
+
+	</div>
 </div>
 
-<footer>
-	<div id="footer">
 
-	     <img src= "images/footer.png" alt="footer"/>
-	</div>
-</footer>
-</body>
-
+	<div id="footer"></br>
+	   <address ><a href="https://www.google.com/maps/place/Meredith+College/				@35.7983206,-78.6889146,16z/data=!3m1!4b1!4m2!3m1!					1s0x89acf5c670c2dbc5:0x179f9c722569698c/"> 3800 Hillsborough 				Street | Raleigh, NC 27607-5298</br>
+       
+          Phone: (919) 760-8600 or 1-800 MEREDITH</address>	
+      	
+	</div>		
+		
+	</body>
 </html>
