c09f14e629f111688737007d2a5058972c91b2dc
Barrett Koster <kosterb@meredith.edu>
2014-03-04 15:16:16 -0500

    This is just to get the content out from under the left menu.  Needs better formatting.

--- jobRequest.php
+++ jobRequest.php
@@ -25,15 +25,22 @@
 
 <?php leftMenu(); ?>
 
+     <div id="content" style="background-color:#EEEEEE;height:200px;width:400px;float:right;">
 <?php
 
-	$jobID=$_POST['career']; 
+	$jobID=addslashes($_GET['career']); 
+	
 	$query="SELECT * FROM Job WHERE id='$jobID';"; 
 	$result=mysql_query($query); 
 	
 	if(noerror($result))
 	{
-		tabledump($result); 
+		$row = mysql_fetch_array($result); 
+		$job = $row['career']; 
+		$des = $row['description']; 
+		
+		echo "<h1>$job</h1> \n"; 
+		echo "<p>$des</p> \n"; 
 	}
 	
 	$q2="SELECT * FROM MajorJob, Major WHERE majorID=id AND jobID='$jobID';";
@@ -41,11 +48,21 @@
 	
 	if(noerror($r2))
 	{
-		tabledump($r2);
+		echo "<h3>Want to be a $job?...Major in:</h3> \n"; 
+		
+		$nr = mysql_num_rows($r2);
+		for($i=0; $i<$nr; $i++)
+		{
+			$row     = mysql_fetch_array($r2); 
+			$major   = $row['major'];
+			$majorID = $row['id']; 
+			
+			echo "<a href='majorRequest.php?major=$majorID'>$major</a> \n"; 
+		}
 	}
 	
 ?>
-
+   </div>
 </body>
 </html>
 
