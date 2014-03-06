3ceae836da10c1e0cdd9138ecbc19568a873a739
Caitlyn Roeckell DeBona <debonaca@email.meredith.edu>
2014-03-06 14:32:07 -0500

    applied style sheet to quiz page

--- quiz.php
+++ quiz.php
@@ -2,20 +2,49 @@
 session_start();
 include("included/openDB.php");
 include("included/tabledump.php");
+include("included/leftMenu.php");
 ?>
 
 <html>
 <head>
+    <link rel= "stylesheet"type="text/css"  href="style.css"  />
+    <title>Career Pathways</title>
 </head>
 <body>
-<h1>Quiz</h1>
-<br>
-<br>
-<p>Look through the list of interests below and drag the things that interest you into the box below. Once you have dragged a few interests into the box below, the majors on the left will change size based off your interests. The larger the major, the more compatiable it is for you.</p>
+    <div id="darkgray"></div>
+	<div id="logo"></div>
+	<div id="lightgray"></div>
+    
+    <!--left Menu-->
+    <div id="links">
+	   <?php leftMenu(); ?>
+    </div>
+    
+    <!--main content-->
+    <div id="text">
+        <!-- <div id="content" style="background-       color:#EEEEEE;height:200px;width:400px;float:right;">-->
+        
+    <h1>Quiz</h1>
+    <br>
+    <br>
+    <p>Look through the list of interests below and drag the things that interest you into the box below. Once you have dragged a few interests into the box below, the majors on the left will change size based off your interests. The larger the major, the more compatiable it is for you.</p>
 
-<h3><i><b>Interests</h3></i></b>
-<br>
-<ul>Problem solving</ul>
+    <h3><i><b>Interests</h3></i></b>
+    <br>
+    <ul>Problem solving</ul>
+        
+         </div>
+ 
+        <div id="footer"></br>
+	       <address >
+	       <a href="https://www.google.com/maps/place/Meredith+College/@35.7983206,
+	   			   -78.6889146,16z/data=!3m1!4b1!4m2!3m1!1s0x89acf5c670c2dbc5:0x179f9c722569698c/">
+	    				3800 Hillsborough Street | Raleigh, NC 27607-5298</br>
+       					Phone: (919) 760-8600 or 1-800 MEREDITH
+       	</address>	
+      	
+	</div>	
+		
 </body>
 </html>
 
