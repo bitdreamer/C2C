23e0525e31933dfe9788af00f73233702fbdcfa8
Caitlyn Roeckell DeBona <debonaca@email.meredith.edu>
2014-03-06 14:20:22 -0500

    created basic quiz page

---
+++ quiz.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+include("included/openDB.php");
+include("included/tabledump.php");
+?>
+
+<html>
+<head>
+</head>
+<body>
+<h1>Quiz</h1>
+<br>
+<br>
+<p>Look through the list of interests below and drag the things that interest you into the box below. Once you have dragged a few interests into the box below, the majors on the left will change size based off your interests. The larger the major, the more compatiable it is for you.</p>
+
+<h3><i><b>Interests</h3></i></b>
+<br>
+<ul>Problem solving</ul>
+</body>
+</html>
+
+
+
