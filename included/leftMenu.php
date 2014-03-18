5d0ad7258379a503ae4a62346b26a052c00b2259
Caitlyn Roeckell DeBona <debonaca@email.meredith.edu>
2014-03-18 15:16:31 -0400

    updated links on left menu

--- included/leftMenu.php
+++ included/leftMenu.php
@@ -1,11 +1,14 @@
 <?php
 	function leftMenu() 
 	{
-		echo"<div id='leftNav' style='background-color:rgb(165, 40, 40)'> \n"; 
+		//echo"<div id='leftNav' style='background-color:rgb(165, 40, 40)'> \n"; 
 		//echo"<img src='images/sideBar.png' alt='logo'/> \n"; 
 		echo"<ul class='links'> \n"; 
 		echo"		<li><a href='http://www.meredith.edu'> Meredith College</a></li> \n"; 
-		echo"		<li><a href='main.html'> Main Page </a></li> \n"; 
+		echo"		<li><a href='http://www.bitlab.meredith.edu/~c2c/'> Main Page </a></li> \n"; 
+        echo"       <li><a href='index.php'> Career PathWays </a></li> \n";
+        echo"       <li><a href='quiz.php'> Major Match Maker</a></li> \n";
+        
 		echo"</ul>	\n"; 	
 		echo"</div> \n"; 
 	}
