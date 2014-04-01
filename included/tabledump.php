e039e1f6bdf1a3ebad1e9c1c519c1e5806f3b99a
McKenzie Bradley <bradleym@email.meredith.edu>
2014-04-01 02:17:38 -0400

    New tabledump function, adds edit button. tabledumpdelt function will soon not be needed, will remove when ready.

--- included/tabledump.php
+++ included/tabledump.php
@@ -130,3 +130,43 @@ function tabledumpdelt( $result)
 	
 }
 ?>
+<?php
+function tabledumpdeltedit( $result)
+{
+   if($result==0)
+   {
+      echo "<b>Error ".mysql_errno().": ".mysql_error()."</b>";
+   }
+   elseif (@mysql_num_rows($result)==0)
+   {
+      echo "<b>There doesn't seem to be anything here... yet.</b><br>";
+   }
+   else
+   {
+   $nf = mysql_num_fields($result);
+   $nr = mysql_num_rows($result);
+      echo "<table border='1'> <thead>";
+         echo "<tr>";
+            for($i=0; $i<$nf; $i++ )
+            {
+               echo "<th>".mysql_field_name($result,$i)."</th>";
+            }
+         echo "</tr>";
+      echo "</thead><tbody>";
+         for($i=0; $i<$nr; $i++ )
+         {
+            echo "<tr>";
+               $row=mysql_fetch_array($result);
+               for( $j=0; $j<$nf; $j++ )
+               { echo "<td>".$row[$j]."</td>"; }
+		//echo "<td> <input type=submit name=id value=$row[0]> </td>";
+		echo "<td> <button id=id name=id value=$row[0]>Delete</button></td>";
+		echo "<td> <button id=ide name=ide value=$row[0]>Edit</button></td>";
+            echo "</tr>";
+         }
+      echo "</tbody></table>";
+   }
+   return $row;
+	
+}
+?>
