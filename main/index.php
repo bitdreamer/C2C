b3d9c97e9e79fadb0ffc17a19473b112328f6cbf
tota241 <jamaltag@email.meredith.edu>
2014-04-02 15:49:42 -0400

    get rid of Divs and refactor CSS

--- main/index.php
+++ main/index.php
@@ -20,16 +20,14 @@
 </head>
 
 <!--logo-->
-<body> 
-	<div id="big_wrapper">
-	
+<body> 	
 <!-- logo part-->	 
 	<header id="top_header">  
 		<section id="logo"></section>
      </header>
      
 <!--Left Menu-->
-<div id="links">
+<aside id="links">
 	<nav id="left_menu">
 	 <ul>
 		<?php leftMenu(); ?>
@@ -37,8 +35,9 @@
 	</nav>
 
 <!-- text-->
-<section id="main_content">
  		<h1 name="mainHeader"> Classroom to Career PathWays</h1>
+
+<section id="main_content">
 
       <article>	 
 		<p name="mainIntro">	
@@ -127,24 +126,23 @@
 		</fieldset>
 	</form>		
 
-</aside>
+</article>
 </section>
  
 	
-</div>
-
 <!--footer-->	
 <footer id="footer">
-	   <div id="address">
+	   <article id="address">
 	   <a href="https://www.google.com/maps/place/Meredith+College/@35.7983206,-78.6889146,16z/data=!3m1!4b1!4m2!3m1!1s0x89acf5c670c2dbc5:0x179f9c722569698c">
 	      3800 Hillsborough Street | Raleigh, NC 27607-5298</a>
 	      </br>
           Phone: (919) 760-8600 or 1-800 MEREDITH
-       </div><!--address-->	   	
+       </article><!--address-->	   	
 	</footer>
 	
 </div>	<!-- big_wrapper-->	
-		
+</aside>
+	
 	</body>
 
 </html>
