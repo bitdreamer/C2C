b0d152bcf3e0b15e4d6dfaf0ebb924980a0be8b1
McKenzie Bradley <bradleym@email.meredith.edu>
2014-04-02 03:41:45 -0400

    Updated leftMenu.php to correctly follow new admin path to find main admin files in main folder.

--- included/leftMenu.php
+++ included/leftMenu.php
@@ -12,12 +12,12 @@
 		//If admin is logged in, display pages that all admins can access, and the logout link
 		if($_SESSION['islogged']!=0)
 		{
-			echo"<li> <a href='../admin/majorAdd.php'>Major</a></li>\n
-				<li> <a href='../admin/jobAdd.php'>Job</a></li>\n
-				<li> <a href='../admin/alumnaAdd.php'>Alumna</a></li>\n
-				<li> <a href='../admin/interestAdd.php'>Interest</a></li>\n
-				<li> <a href='../admin/opportunityAdd.php'>Opportunities</a></li>\n
-				<li> <a href='../admin/linkAdd.php'>Links</a></li>\n";
+			echo"<li> <a href='../main/majorAdd.php'>Major</a></li>\n
+				<li> <a href='../main/jobAdd.php'>Job</a></li>\n
+				<li> <a href='../main/alumnaAdd.php'>Alumna</a></li>\n
+				<li> <a href='../main/interestAdd.php'>Interest</a></li>\n
+				<li> <a href='../main/opportunityAdd.php'>Opportunities</a></li>\n
+				<li> <a href='../main/linkAdd.php'>Links</a></li>\n";
 
 			//If full-admin is logged in, dislay main_register page link
 			if($_SESSION['accessLv'] == 2)
