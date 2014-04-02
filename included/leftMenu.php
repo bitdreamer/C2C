64cd107636d47994ecbe88092a9bc2384ee5940d
Allison G. Martinez-Arocho <allygmartinez@gmail.com>
2014-04-02 10:39:04 -0400

    Fixed bad pointers in leftMenu.php and deleted mainMenu.php

--- included/leftMenu.php
+++ included/leftMenu.php
@@ -4,20 +4,20 @@
 		//Always display the main links, and the Admin list header
 		echo"<nav>\n
 			<ul>\n
-			<li><a href='../index.php'>Career PathWays</a></li>\n
-        		<li><a href='../main/quiz.php'>Major Match Maker</a></li>\n
+			<li><a href='index.php'>Career PathWays</a></li>\n
+        		<li><a href='quiz.php'>Major Match Maker</a></li>\n
 
 			<li><strong>Admin</strong></li>\n";
 		
 		//If admin is logged in, display pages that all admins can access, and the logout link
 		if($_SESSION['islogged']!=0)
 		{
-			echo"<li> <a href='../main/majorAdd.php'>Major</a></li>\n
-				<li> <a href='../main/jobAdd.php'>Job</a></li>\n
-				<li> <a href='../main/alumnaAdd.php'>Alumna</a></li>\n
-				<li> <a href='../main/interestAdd.php'>Interest</a></li>\n
-				<li> <a href='../main/opportunityAdd.php'>Opportunities</a></li>\n
-				<li> <a href='../main/linkAdd.php'>Links</a></li>\n";
+			echo"<li> <a href='majorAdd.php'>Major</a></li>\n
+				<li> <a href='jobAdd.php'>Job</a></li>\n
+				<li> <a href='alumnaAdd.php'>Alumna</a></li>\n
+				<li> <a href='interestAdd.php'>Interest</a></li>\n
+				<li> <a href='opportunityAdd.php'>Opportunities</a></li>\n
+				<li> <a href='linkAdd.php'>Links</a></li>\n";
 
 			//If full-admin is logged in, dislay main_register page link
 			if($_SESSION['accessLv'] == 2)
